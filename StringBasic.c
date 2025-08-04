fd88c4fa30d51efa1f4d2fad0ee8a5eab9b658d7
Anuj kr Sharma <188915825+anujkrsharmaa@users.noreply.github.com>
2025-08-04 10:14:12 +0530

    Strings

---
+++ StringBasic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // char arr[5]={'a', 'b', 'c', 'd', 'e'};
+    // printf("%c", arr[3]); // prints 'a'
+    char ch = 'a';
+    int x = (int)ch; // typecasting char to int
+    printf("%d\n", x); // prints 97, ASCII value of 'a'
+    return 0;
+}
+
+
